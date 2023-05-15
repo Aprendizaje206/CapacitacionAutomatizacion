ac64a6e96e8df67ee462b6a3f12e5928208bd711
DanielTiqueMoreno <daniel.tique@segurosbolivar.com>
2023-05-15 09:55:34 -0500

    Ajustes.

--- src/Operaciones.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public abstract class Operaciones {
-
-    public abstract double sumar(double a, double b);
-    public abstract double restar(double a, double b);
-    public abstract double multiplicar(double a, double b);
-    public abstract double dividir(double a, double b);
-}
