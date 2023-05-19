772dc7e8b52587672727c784c9ad617bdecc764e
laguirrev <david.aguirre.qa@gmail.com>
2023-05-19 08:11:33 -0500

    Ejercicio 1 de automatizacion

--- src/test/java/getting_started/FirstScriptTest.java
+++ src/test/java/getting_started/FirstScriptTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
@@ -22,7 +23,7 @@ public class FirstScriptTest {
     public void setUp() {
 
         driver = new ChromeDriver();
-        driver.get("https://google.com");
+        driver.get("https://the-internet.herokuapp.com/login");
 
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
@@ -34,18 +35,53 @@ public class FirstScriptTest {
     }
 
     @Test
-    public void firstTest() {
-        String title = driver.getTitle();
-        Assert.assertEquals("Google", title);
+    public void firstTest() throws InterruptedException {
 
-        WebElement searchBox = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.name("q")));
-        WebElement searchButton = driver.findElement(By.name("btnK"));
 
-        searchBox.sendKeys("Selenium");
-        searchButton.click();
+        WebElement usuario = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"username\"]")));
+         usuario.sendKeys("tomsmith");
 
-        searchBox = driver.findElement(By.name("q"));
-        String value = searchBox.getAttribute("value");
-        Assert.assertEquals("Selenium", value);
+        WebElement pass = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("password")));
+        pass.sendKeys("SuperSecretPassword!");
+
+        WebElement boton = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"login\"]/button/i")));
+        boton.click();
+
+         Thread.sleep(2000);
+
+
+        WebElement titulo = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),'You logged into a secure area!')]")));
+        String textoInicial = titulo.getText().trim();
+        textoInicial=textoInicial.replaceAll("\n", "");
+        textoInicial=textoInicial.replace("×","");
+        textoInicial=textoInicial.trim();
+        System.out.println(textoInicial);
+
+        Thread.sleep(1000);
+
+       String textoComparar1 = "You logged into a secure area!";
+        Assert.assertEquals(textoComparar1,textoInicial);
+
+
+        WebElement BotonSalir = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"content\"]/div/a")));
+        BotonSalir.click();
+
+        Thread.sleep(2000);
+
+        WebElement tituloFinal = new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("flash")));
+
+
+        String textoFinal = tituloFinal.getText().trim();
+        textoFinal=textoFinal.replaceAll("\n", "");
+        textoFinal=textoFinal.replace("×","");
+        textoFinal=textoFinal.trim();
+        System.out.println(textoFinal);
+
+
+
+        String textoComparar2 = "You logged out of the secure area!";
+        Assert.assertEquals(textoComparar2,textoFinal);
+
+
     }
 }
